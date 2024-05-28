4b06b778cfcd957183de9eecffccde1085b22bf8
rishihk <hk.hrishi30@gmail.com>
2024-05-28 14:17:14 -0500

    hello gophers

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ My journey learning GoLang fundementals.
 
 ## What I learnt
 
-
+what are go modules?
+how to setup go modules?
+why package main at the top?
+whats entry point for go?
+go run . to compile and run. why the dot?
---
+++ main.go
@@ -0,0 +1,8 @@
+package main // package identifier indicates that we are creating a source file for something we are going to compile later
+
+// fmt package allows us to work with strings
+import "fmt"
+
+func main(){ // entry point in go 
+	fmt.Println("Hello Gophers!")
+}

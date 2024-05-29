78f36c0229a7d2f932b55f552971a499465ccd23
rishihk <hk.hrishi30@gmail.com>
2024-05-29 11:51:05 -0500

    add go module

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
+// go modules help manage dependencies in Go projects. Specifies dependencies and required versions. (go mod init)
+// gopath is env variable pointing to location of go workspace containing all neccessary files for Go development.
+// goroot points to the location of go installation directory (SDK).
 package main // package identifier indicates that we are creating a source file for something we are going to compile later
 
 // fmt package allows us to work with strings
 import "fmt"
 
-func main(){ // entry point in go 
+func main() { // entry point in go
 	fmt.Println("Hello Gophers!")
 }
